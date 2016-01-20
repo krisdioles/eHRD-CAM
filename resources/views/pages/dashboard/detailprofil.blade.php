3ec543e997b6fc6b0966f72170538bd1a1552b6d
krisdioles <krisdio.les@gmail.com>
2016-01-20 20:35:45 +0700

    button fixing

--- resources/views/pages/dashboard/detailprofil.blade.php
+++ resources/views/pages/dashboard/detailprofil.blade.php
@@ -75,7 +75,7 @@
 				</tr>
 			</table>
 			<br>
-			<a class="btn btn-default" href="/eHRD-CAM/public/pegawai" role="button">Back</a>
+			<a class="btn btn-default" href="/eHRD-CAM/public/dashboard" role="button">Back</a>
 		</div>
 	</div>
 </div>
